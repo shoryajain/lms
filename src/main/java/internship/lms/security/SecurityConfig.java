c17c90706bdc37a9ac53e2076009fe9795623221
Shorya <shoryajain1999@gmail.com>
2018-06-28 22:07:15 +0530

    BasicLogin

---
+++ src/main/java/internship/lms/security/SecurityConfig.java
@@ -0,0 +1,27 @@
+package internship.lms.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	@Override
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("shorya")
+				.password("trying").roles("USER");
+	}
+
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		http.antMatcher("/**").authorizeRequests().anyRequest().hasRole("USER")
+				.and().formLogin().loginPage("/login.jsp").defaultSuccessUrl("/test.html", true)
+				.permitAll().and().logout().permitAll();
+
+	}
+
+}
+
